ee4265675896baa4d7e75213313cc78ac14924a0
tiBitZ <freddy.lee96@gmail.com>
2015-02-06 21:14:22 -0500

    Initialize repository

--- app/src/main/java/com/eventr/eventr/LoginActivity.java
+++ app/src/main/java/com/eventr/eventr/LoginActivity.java
@@ -6,19 +6,19 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class HomeActivity extends ActionBarActivity {
+public class LoginActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_login);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
+        getMenuInflater().inflate(R.menu.menu_login, menu);
         return true;
     }
 
