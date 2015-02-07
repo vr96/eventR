754e4e17dfac0ab0dcbc92a42b8170710cae4650
tiBitZ <freddy.lee96@gmail.com>
2015-02-07 01:41:26 -0500

    Add seekbar counter

--- app/src/main/java/com/eventr/eventr/StatusFragment.java
+++ app/src/main/java/com/eventr/eventr/StatusFragment.java
@@ -5,13 +5,63 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Switch;
+import android.widget.TextView;
 
 
 public class StatusFragment extends Fragment {
+
+    protected TextView mBubbleTextView;
+    protected SeekBar mSeekBar;
+    protected Switch mPrivacySwitch;
+    protected EditText mStatusText;
+
+    protected int radius;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_status, container, false);
+        mBubbleTextView = (TextView)rootView.findViewById(R.id.bubbleSizeTextView);
+        mSeekBar = (SeekBar)rootView.findViewById(R.id.seekBar);
+        mPrivacySwitch = (Switch)rootView.findViewById(R.id.privacySwitch);
+        mStatusText = (EditText)rootView.findViewById(R.id.statusEditText);
+
+        //similar to data call: remove once in parse
+        radius = 5;
+        mSeekBar.setProgress(radius * 20);
+        mBubbleTextView.setText("Search Radius: " + String.valueOf(radius) + " miles");
+
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,
+                                          boolean fromUser) {
+                // TODO Auto-generated method stub
+                radius = progress/20;
+                mBubbleTextView.setText("Search Radius: " + String.valueOf(radius) + " miles");
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                // TODO Auto-generated method stub
+            }
+        });
+
         return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+
     }
 }
